14a9336e8bba96ce19a026dece6893d3991a0039
Catrin <catrin.lam@gmail.com>
2025-01-17 11:03:19 +0000

    Add post detail template and update view to include coder information

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,4 @@
-from django.shortcuts import render
+from django.shortcuts import render, get_object_or_404
 from django.views import generic
 from .models import Post
 
@@ -23,4 +23,9 @@ def post_detail(request, slug):
     """
     queryset = Post.objects.filter(status=1)
     post = get_object_or_404(queryset, slug=slug)
-    return render(request, "blog/post_detail.html", {"post": post},)
+    return render(
+        request,
+        "blog/post_detail.html",
+        {"post": post,
+            "coder": "Catrin Lam"},
+    )
